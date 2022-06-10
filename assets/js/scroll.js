4521ad7c4744f694860bf41645ea5757de7cc418
Juan Manuel Maroñas <jmmaronas@gmail.com>
2022-06-10 08:50:08 -0300

    mejoras en los logos

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -2,19 +2,19 @@ const containerScrollCer=document.getElementById("scrollCer")
 const btnCerRight= document.getElementById("scrollCerRight")
 const btnCerLeft= document.getElementById("scrollCerLeft")
 
-btnCerRight.addEventListener("click", ()=>containerScrollCer.scrollLeft -= 100)
-btnCerLeft.addEventListener("click", ()=>containerScrollCer.scrollLeft += 100)
+btnCerRight.addEventListener("click", ()=>containerScrollCer.scrollLeft -= 300)
+btnCerLeft.addEventListener("click", ()=>containerScrollCer.scrollLeft += 300)
 
 const containerScrollCli=document.getElementById("scrollCli")
 const btnCliRight= document.getElementById("scrollCliRight")
 const btnCliLeft= document.getElementById("scrollCliLeft")
 
-btnCliRight.addEventListener("click", ()=>containerScrollCli.scrollLeft -= 100)
-btnCliLeft.addEventListener("click", ()=>containerScrollCli.scrollLeft += 100)
+btnCliRight.addEventListener("click", ()=>containerScrollCli.scrollLeft -= 300)
+btnCliLeft.addEventListener("click", ()=>containerScrollCli.scrollLeft += 300)
 
 const containerScrollCol=document.getElementById("scrollCol")
 const btnColRight= document.getElementById("scrollColRight")
 const btnColLeft= document.getElementById("scrollColLeft")
 
-btnColRight.addEventListener("click", ()=>containerScrollCol.scrollLeft -= 100)
-btnColLeft.addEventListener("click", ()=>containerScrollCol.scrollLeft += 100)
+btnColRight.addEventListener("click", ()=>containerScrollCol.scrollLeft -= 300)
+btnColLeft.addEventListener("click", ()=>containerScrollCol.scrollLeft += 300)
